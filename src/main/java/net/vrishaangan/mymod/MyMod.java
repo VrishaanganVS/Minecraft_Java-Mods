88cc47ed0c038769ba1d129b923398229a1938da
VrishaanganVS <teknobot24@gmail.com>
2024-02-11 20:58:31 +0530

    registered the item sapphire
    registered in the item in the creative menu and made sure it is vissible in the hand slot using appropriate codes

--- src/main/java/net/vrishaangan/mymod/MyMod.java
+++ src/main/java/net/vrishaangan/mymod/MyMod.java
@@ -1,6 +1,8 @@
 package net.vrishaangan.mymod;
 
 import com.mojang.logging.LogUtils;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -13,6 +15,7 @@ import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.vrishaangan.mymod.item.DropItem;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
@@ -28,6 +31,8 @@ public class MyMod
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
+        DropItem.register(modEventBus); // this ensures that our deferredregister is
+        // properly registered and our items are actually added to the game
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
         // Register ourselves for server and other game events we are interested in
@@ -45,7 +50,12 @@ public class MyMod
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
-
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(DropItem.SAPPHIRE);
+            /* This is to add the sapphire into the game in the creative menu but
+            it still dosent have a assigned name and texture.
+            */
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
---
+++ src/main/java/net/vrishaangan/mymod/item/DropItem.java
@@ -0,0 +1,31 @@
+package net.vrishaangan.mymod.item;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.EventBus;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.vrishaangan.mymod.MyMod;
+
+public class DropItem {
+    public static final DeferredRegister<Item> ITEM =
+            DeferredRegister.create(ForgeRegistries.ITEMS, MyMod.MOD_ID);
+
+    public static final RegistryObject<Item> SAPPHIRE = ITEM.register("sapphire",
+            () -> new Item(new Item.Properties()));
+    // This is to register a custom Item named sapphire with the name sapphire
+    /* () -> new Item(new Item.Properties()) this is a lambda expression defining how to
+    create an instance of the item. Here, it creates a new Item instance with default properties
+     */
+
+
+    public static void register(IEventBus eventBus)
+    {
+        ITEM.register(eventBus);
+    }
+
+
+}
+
+
